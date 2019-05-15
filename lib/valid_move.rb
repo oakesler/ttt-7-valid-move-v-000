9363d1ee9b835e75833eda93a76077df8ea6e86b
Olivia Kesler <oakesler@protonmail.ch>
2019-05-15 22:43:52 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,11 @@
-# code your #valid_move? method here
-
-
-# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def valid_move?(board, index)
+  if (0...8) === index && position_taken? == true
+        true
+    else
+        false
+    end
+end
+  
+  
+def position_taken?()
+end

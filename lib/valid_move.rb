d0ca8a9475d1fcc88c5ca6fb5c081f9d30290673
Olivia Kesler <oakesler@protonmail.ch>
2019-05-15 22:45:19 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -7,5 +7,11 @@ def valid_move?(board, index)
 end
   
   
-def position_taken?()
+def position_taken?(board, index)
+  x = board[index]
+  if x == " " || x == "" || x == nil
+    false
+  else x == "X" || x = "O"
+    true
+  end
 end
